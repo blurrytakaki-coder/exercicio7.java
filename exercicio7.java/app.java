8422ff09bddcca48faddd313fba8341918ef7387
blurrytakaki-coder <blurrytakaki@gmail.com>
2026-03-24 21:22:29 -0300

    Update app.java

---
+++ exercicio7.java/app.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+double nota;
+
+System.out.print("Digite a nota 0 a 100: ");
+nota = scanner.nextDouble();
+
+if ("se a nota for de 0 a 100") {
+    System.out.println("nota " + " conceito ");
+} else {
+    System.out.println("nota " + " tente novamente ");
+}
+
+scanner.close(); 
+}
